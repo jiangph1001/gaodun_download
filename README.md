494aed3e51729fd033634b09165be4b59cf294ac
jiangph2016 <jiangph1001@qq.com>
2020-12-01 12:51:15 +0800

    run in mac

--- README.md
+++ README.md
@@ -2,21 +2,38 @@
 
 
 
+# 依赖
+
+```
+pip install Crypto
+```
+
 # 运行
+
+按F12打开chrome的开发者模式，找到Network选项，然后刷新视频播放的页面，下面会出现一些记录
+
+<img src="https://gitee.com/greynius/picbed/raw/master/img/20201201113655.png"/>
+
+右键选择任意一条记录，找到`Save all as HAR with content`，保存到程序目录下的`HAR`文件夹中，例如此处命名为`test.har`
+
+<img src="https://gitee.com/greynius/picbed/raw/master/img/20201201113231.png" style="zoom: 67%;" />
+
+然后执行命令即可开始下载
 
 ```
 python3 download.py
 ```
 
-执行后会自动通过记事本打开`video.txt`文件，此时手动将m3u8信息写入video.txt即可（覆盖已有的就行）
-
-**目前仅支持windows下运行**
-
-m3u8信息目前需要手动获取，通过chrome的开发者模式找到，样例如`video.txt`
 
 
+# 注意
 
-# 原理
-
-下载多个ts视频流，下载完成后统一执行合并，默认视频都是AES-128加密的，所以还需要解密
-
+- har目录下**不要放**不是har的文件
+- 文件名必须以`.har`结尾
+- 最后生成的视频文件名与har文件名和时间有关，例如
+  - 保存的是`test.har`
+  - 最后生成的视频文件是`test.ts`
+- 可以往har文件夹中可以放入尽可能多的har文件，会依次下载
+- 下载成功后，har文件会自动删除
+- 可能存在因网络问题导致下载失败的情况，重新运行即可
+- 代码中设定的多进程，一次可以同时下载3个文件（但是可以放很多个文件进去，都能下载完）
--- download.py
+++ download.py
@@ -1,105 +1,71 @@
 # -*- coding:utf-8 -*-  
 import os
-import requests
+import requests,time
 from Crypto.Cipher import AES
 from binascii import b2a_hex, a2b_hex
-import uuid
-import shutil
+import multiprocessing
+import uuid,json
+import base64
+def parse_har(har_file_name):
+    file = open(har_file_name,'r')
+    content = file.read()
+    my_har_origin = json.loads(content)
+    m3u8 = None
+    IV = None
+    for req in my_har_origin['log']['entries']:
+        #req_json = json.loads(req)
+        if 'request' in req:
+            if 'url' in req['request']:
+                if "src/get?id=" in req['request']['url']:
+                    m3u8 = req['response']['content']['text']
+                if "authorize?id=" in req['request']['url']:
+                    IV = req['response']['content']['text']
+    file.close()
+    return m3u8,IV
 
-### 以下引用仅为了打包exe设置
-import queue
-
-### end
-
-def read_m3u8(filename,uuid_str):
-    m3u8_file = open(filename,"r")
-    all_content = m3u8_file.read();
+def read_m3u8(m3u8,key,filename):
+    all_content = base64.b64decode(m3u8).decode('utf-8')
+    key  = base64.b64decode(key)
+    cryptor = AES.new(key, AES.MODE_CBC, key) 
     if "#EXTM3U" not in all_content:
-        raise BaseException("非M3U8的链接")
-       
+        print("not found video in " + filename)
+        return 
     file_line = all_content.split("\n")
-    key = ""
-    ## name 作为缺省值，可设置可不设置
-    name = "new" 
-    for index, line in enumerate(file_line): # 第二层
-        if "#NAME" in line:
-            name = line.split('=')[1]
-            print("文件名是：",name)
-        if "#EXT-X-KEY" in line:  # 找解密Key
-            method_pos = line.find("METHOD")
-            comma_pos = line.find(",")
-            method = line[method_pos:comma_pos].split('=')[1]
-            print("Decode Method：", method)
-            
-            uri_pos = line.find("URI")
-            quotation_mark_pos = line.rfind('"')
-            key_path = line[uri_pos:quotation_mark_pos].split('"')[1]
-            
-            key_url = key_path # 拼出key解密密钥URL
-            res = requests.get(key_url)
-            key = res.content
-            print("key_url：",key_url)
-            print("key:",key)
-            if(len(key)==0):
-                print("秘钥解析异常")
-                #raise BaseException("解密秘钥请求异常")
-        if os.path.exists(uuid_str) == False:
-            os.mkdir(uuid_str)
+    time_str = time.strftime('%d-%H%M%S', time.localtime(time.time()))
+    ts_name = "download/{}-{}.ts".format(time_str,filename)
+    writer = open(ts_name, 'wb')
+    for index, line in enumerate(file_line): # 第二层 
         if "EXTINF" in line: # 找ts地址并下载
             pd_url = file_line[index + 1] # 拼出ts片段的URL
-            #print(pd_url)
             try:
                 res = requests.get(pd_url)
+                print(filename,pd_url)
             except Exception as e:
                 print(e)
-                print("开始回退操作，此过程将会清除已下载的视频流，请重新添加下载")
-                delete_file(uuid_str)
-                return
-            c_fule_name = file_line[index + 1].rsplit("/", 1)[-1]
-            ts_name = uuid_str+"/"+c_fule_name + ".tmp"
-            print(ts_name)
-            if len(key): # AES 解密
-                cryptor = AES.new(key, AES.MODE_CBC, key)  
-                with open(ts_name, 'ab') as f:
-                    f.write(cryptor.decrypt(res.content))
-            else:
-                with open(c_fule_name, 'ab') as f:
-                    f.write(res.content)
-                    f.flush()
-        if "#EXT-X-ENDLIST" in line:
-            merge_file(name,uuid_str)
-            name = "new"
-
-def delete_file(uuid_str):
-    # 下载失败时，删除已经下载的文件
-    #os.chdir("./"+uuid_str)
-    print("当前位于 %s" % (os.getcwd()) )
-    shutil.rmtree(uuid_str,True)
-    if os.path.exists(uuid_str):
-        os.rmdir(uuid_str)
-    print("删除完成")
-
-def merge_file(file_name,uuid_str):
-    if file_name == "new":
-        file_name = uuid_str
-    os.chdir("./"+uuid_str)
-    #print("进入到 %s" % (os.getcwd()) )
-    copy_cmd = 'copy /b *.tmp "'+ file_name + '.ts"'
-    print(copy_cmd)
-    os.system(copy_cmd)
-    mv_cmd = 'move "' + file_name + '".ts ../'
-    os.system(mv_cmd)    
-    #os.rename("new.tmp", file_name+".ts")
-    print(file_name,"完成")
-    os.chdir("../")
-    delete_file(uuid_str)
+                print("请求出错，需要重新下载")
+                writer.close()
+                os.system("rm -r " + ts_name)
+                return 
+            writer.write(cryptor.decrypt(res.content))
+    print("{} finish download".format(filename))
+    writer.close()
+    os.system("mv {} download/{}.ts".format(ts_name,filename))
+    os.system("rm -r " + 'har/'+har)
+    return 0
 
 if __name__ == '__main__': 
-    print("将m3u8信息放入video.txt中")
-    os.system("notepad video.txt")
-    # uuid 作为该任务唯一的标识符
-    uuid_str = str(uuid.uuid4()).split("-")[-1]
-    print(uuid_str)
-    read_m3u8("video.txt",uuid_str)
-    #delete_file("cbc07bcebfa2")
-    #merge_file()
+    har_list = os.listdir('har/')
+    print(har_list)
+    pool = multiprocessing.Pool(processes = 3)
+    for har in har_list:
+        file_without_suffix = har.rsplit('.')[0]
+        print(file_without_suffix)
+        try:
+            m3u8,key = parse_har('har/'+har)
+        except:
+            print("{} parse error".format(har))
+        else:
+            pool.apply_async(read_m3u8,(m3u8,key,file_without_suffix))
+    pool.close()
+    pool.join()
+            
